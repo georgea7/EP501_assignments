eb739b4c8d2ac8fe4cc89e4bfe18d17c5ccaec0c
georgea7 <70706419+georgea7@users.noreply.github.com>
2020-12-01 19:58:14 -0500

    HW5 P1

--- assignments/HW5/George_Project5.m
+++ assignments/HW5/George_Project5.m
@@ -13,25 +13,28 @@ close all
 I   = 10;               %(A)
 mu0 = 4*pi*10^(-7);     %(H/m)
 a   = 0.004;            %(m)
-x = linspace(-3*a,3*a,100);
-y = linspace(-3*a,3*a,100);
+
+lx=100;                 %size of x
+ly=100;                 %size of y
+x = linspace(-3*a,3*a,lx);
+y = linspace(-3*a,3*a,ly);
 [X,Y] = meshgrid(x,y);
 
 Bx = zeros(100,100);
 By = zeros(100,100);
 
-for i=1:100
-    for j=1:100
-        cond = sqrt(x(i)^2+y(j)^2);
+for i=1:lx
+    for j=1:ly
+        cond = sqrt(x(i)^2+y(j)^2); %condition for piecewise function
         if cond<a
             Bx(i,j) = (mu0*I/(2*pi*a^2))*cond*(-(y(j)/cond));
             By(i,j) = (mu0*I/(2*pi*a^2))*cond*(x(i)/cond);
         else
             Bx(i,j) = (mu0*I/(2*pi))*(1/cond)*(-(y(j)/cond));
             By(i,j) = (mu0*I/(2*pi))*(1/cond)*(x(i)/cond);
-        end
-    end
-end
+        end %if
+    end %for
+end %for
 
 %1-a
 %plot
@@ -43,9 +46,9 @@ ylim([-3*a,3*a]);
 xlabel('x');
 ylabel('y');
 title('B_x');
-colorbar 
+colorbar
 shading flat
-hold on 
+hold on
 %By
 figure(2)
 pcolor(X,Y,By);
@@ -54,10 +57,44 @@ ylim([-3*a,3*a]);
 xlabel('x');
 ylabel('y');
 title('B_y');
-colorbar 
+colorbar
 shading flat
 
 %1-b
 figure(3)
 quiver(X,Y,Bx',By');
 title('B Quiver plot');
+
+%1-c
+dBx=Bx(2)-Bx(1);
+dBy=By(2)-By(1);
+gradBx=zeros(size(Bx));
+gradBy=zeros(size(By));
+
+gradBx(1,1)=(Bx(2,2)-Bx(1,1))/dBx;
+for i=2:lx-1
+    for j=2:lx-1
+    gradBx(j,i)=(Bx(j,i+1)-Bx(j-1,i-1))/2/dBx;    %\partial/\partial x
+    end %for
+end %for
+gradBx(ly,lx)=(Bx(ly,lx)-Bx(ly-1,lx-1))/dBx;
+
+%% Problem 2
+
+%Initiation
+Q = 1;              %(C)
+a = 1;              %(m)
+Eps0 = 8.854*10^-12;%(F/m)
+
+for i=1:lx
+    for j=1:ly
+        for k=1:lz
+            cond = sqrt(x^2+y^2+x^2); %condition for piecewise function
+            if cond<a
+            phix
+            else
+            phix
+            end %if
+        end%for
+    end%for
+end%for
